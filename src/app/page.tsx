7087e6ec29b8135942407e26263f9d967464ec4e
Samyak Piya <samyakpiya@gmail.com>
2024-08-10 08:23:46 -0400

    force dynamic + vscode stuff

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
-import Link from "next/link";
 import { db } from "~/server/db";
+
+export const dynamic = "force-dynamic";
 
 const mockUrls = [
   "https://utfs.io/f/c9736860-3878-4848-9a95-bd5a787fe182-6kk2kw.jpg",
